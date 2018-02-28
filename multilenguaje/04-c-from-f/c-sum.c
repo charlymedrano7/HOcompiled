d16a33a45532740744ee07ddcdff9cda17f2f9e1
Charly <charlymedrano7@gmail.com>
2018-02-28 20:34:40 -0300

    Ejercicio 4 resuelto, con archivo compilacion.txt que describe la compilacion

--- multilenguaje/04-c-from-f/c-sum.c
+++ multilenguaje/04-c-from-f/c-sum.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
 #include "c-sum.h"
 
-int sum_abs(int *in, int num) {
-   int i,sum;
+void sum_abs_(int *in, int *num, int *sum) { //Cambio int por void para sacarme de encima el return (fortran no permite que
+					    //le devuelvan nada, solo puede cambiar el valor de las variables que le pasan
+		     			    //también le tengo que decir a C que las variables que vienen de fortran son
+					    //punteros, porque fortran trabaja todo con punteros. Para eso son los * de las
+					    //variables. También necesito "enmascarar" la función de C con el sufijo "_"
+					    //para que fortran la vea como una subrutina.
+   int i;				    //ya no necesito declarar sum
 
-   for (i=0,sum=0; i < num; ++i) {
-       sum += abs(in[i]);
+   for (i=0,*sum=0; i < *num; ++i) {        //vuelvo a usar el * para obtener el valor asignado al puntero, porque acá
+       *sum += abs(in[i]);                  //necesito operar con el valor correspondiente al puntero.
    }
-   return sum;
+//   return &asum;                          //El return lo puedo (y debo, por culpa de fortran) porque puse el void arriba
 }
  
